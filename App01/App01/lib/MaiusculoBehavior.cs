a55676527f56e91eba5d9aa7855afba3bcb46c2a
Marcelo Pereira Santos <lemarcelo@outlook.com>
2019-10-24 17:46:19 -0300

    Add project files.

---
+++ App01/App01/lib/MaiusculoBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+
+namespace App01.lib
+{
+    public class MaiusculoBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += MaiusculoAction;
+        }
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= MaiusculoAction;
+        }
+
+        private void MaiusculoAction(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.NewTextValue))
+            {
+                ((Entry)sender).Text = e.NewTextValue.ToUpper();
+
+            }
+        }
+    }
+}
